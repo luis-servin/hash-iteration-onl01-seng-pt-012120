cd6f6e7857ce5ec1f7482bbf048fde43a56e7afc
Luis Alberto Servin <lservin2015@gmail.com>
2020-02-03 20:28:06 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,6 +13,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   age_appropriate_birthday do |kids_name, age|
     if age <= 12 
-      
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
 
 
